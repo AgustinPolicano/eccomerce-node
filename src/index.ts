edb5df42663638651f93798ed6e6338e3653f867
Agustin Policano <policanoagus@gmail.com>
2023-03-01 11:28:36 -0300

    add mongoose for db and add connection with atlas db

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,22 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+import mongoose from "mongoose";
+
+const passwordDB = "DK0dW5yXKf421VU2";
+
 import allProducts from "./routes/products";
 
 const PORT = 3000;
+
+async function connectDB() {
+  const db = await mongoose.connect(
+    "mongodb+srv://policanoagus:DK0dW5yXKf421VU2@cluster0.tfjuvya.mongodb.net/?retryWrites=true&w=majority"
+  );
+  console.log("database connected to", db.connection.db.databaseName);
+}
+
+connectDB();
 
 app.get("/ping", (req, res) => {
   console.log("ping");
@@ -12,8 +25,7 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-app.use('/api/products', allProducts);
-
+app.use("/api/products", allProducts);
 
 app.listen(PORT, () => {
   console.log("server running on port ", PORT);
