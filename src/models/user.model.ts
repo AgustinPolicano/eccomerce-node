98f4e22a1378bdf91523d7cdf6a44c47908f6870
Agustin Policano <aguspoli123456@gmail.com>
2023-03-02 13:40:30 -0300

    add validators in user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema ({
-    userName: {type: String, required: true},
-    email: {type: String, required: true},
+    userName: {type: String, required: true, minLenght: 6},
+    email: {type: String, required: true, minLenght: 6},
     password: {type: String, required: true, minLenght: 6}
 })
 
